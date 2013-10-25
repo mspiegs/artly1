c0237696c09e8f1f0a99d941fbd467ff02477093
mspiegs <matt@mycaseinc.com>
2013-10-25 16:50:13 -0700

    added index route

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Artly::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'art_pieces#index'
 
   # See how all your routes lay out with "rake routes"
 
