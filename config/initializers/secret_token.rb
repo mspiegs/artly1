bcb4afa9a9b134974c9bde1173a46afd3dadfe86
mspiegs <matt@mycaseinc.com>
2013-10-25 15:35:34 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artly::Application.config.secret_token = '20d9f240775e561ba2b3a8bd8e5d08a794b12d0b4286a85d228c26bf55a4f276a14c5a60a09f3b84fa5d35aaa1bb6a3125ebeeffc609d61ef0b5015e4ac8d2c7'
